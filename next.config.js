df7e8feb6651758a5a9feb1144d7a4dfc264fa1f
Luobin <luobin@supeq.com>
2018-12-08 18:11:07 +0800

    chore: add css file process

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 // next.config.js
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-    webpack(config, options) {
-        return config
-    }
+const css = require('@zeit/next-css')
+const sass = require('@zeit/next-sass')
+const withPlugins = require('next-compose-plugins')
+
+const config = [
+    css({
+
+    }),
+    sass({
+        // cssModules: false,
+        // cssLoaderOptions: {
+        //     importLoaders: 1
+        //     // localIdentName: '[local]___[hash:base64:5]'
+        // },
+        // webpack(config, options) {
+        //     return config
+        // }
+    })
+]
+module.exports = withPlugins([...config], {
+    pageExtensions: ['jsx', 'js']
 })
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import 'moment'
+import 'moment/locale/zh-cn'
 import Head from 'next/head'
+// import { DatePicker } from 'antd'
 import '../style/common.scss'
+import '../style/s.css'
 
-export default () => {
-    return <div>
+export default () => (
+    <div>
         <Head>
             <title>Nice react app with SSR</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         </Head>
         <p>Hello world!</p>
+        {/*<DatePicker></DatePicker>*/}
     </div>
-}
+)
