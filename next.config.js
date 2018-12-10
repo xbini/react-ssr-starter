2421f5abc75ce00db07528f04121828604ca491e
x-thanatos <xluobin@163.com>
2018-12-10 22:53:07 +0800

    chore: add transpile-module plugin to fix css flie in node_modules problem

--- next.config.js
+++ next.config.js
@@ -1,23 +1,39 @@
 // next.config.js
+const transpileModules = require('next-plugin-transpile-modules')
 const css = require('@zeit/next-css')
 const sass = require('@zeit/next-sass')
+const images = require('next-images')
 const withPlugins = require('next-compose-plugins')
-
 const config = [
-    css({
-
-    }),
-    sass({
-        // cssModules: false,
-        // cssLoaderOptions: {
-        //     importLoaders: 1
-        //     // localIdentName: '[local]___[hash:base64:5]'
-        // },
-        // webpack(config, options) {
-        //     return config
-        // }
-    })
+    [transpileModules, {
+        transpileModules: ['antd']
+    }],
+    [images, {}],
+    [css, {
+        cssLoaderOptions: {
+            includePaths: ['node_modules/antd']
+        }
+    }],
+    [sass, {}]
 ]
-module.exports = withPlugins([...config], {
-    pageExtensions: ['jsx', 'js']
-})
+const nextConfig = {
+    exportPathMap: {
+
+    },
+    pageExtensions: ['jsx', 'js'],
+    webpack(config, options) {
+        // config.module.rules.push({
+        //     test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        //     use: {
+        //         loader: 'url-loader',
+        //         options: {
+        //             limit: 100000,
+        //             name: '[name].[ext]'
+        //         }
+        //     }
+        // })
+        return config
+    }
+
+}
+module.exports = withPlugins(config, nextConfig)
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import 'moment'
 import 'moment/locale/zh-cn'
 import Head from 'next/head'
 // import { DatePicker } from 'antd'
+import DatePicker from 'antd/lib/date-picker';  // 加载 JS
+import Button from 'antd/lib/button'
+import 'antd/lib/button/style/css'
+import 'antd/lib/date-picker/style/css';
 import '../style/common.scss'
 import '../style/s.css'
 
@@ -12,6 +16,7 @@ export default () => (
             <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         </Head>
         <p>Hello world!</p>
-        {/*<DatePicker></DatePicker>*/}
+        <DatePicker></DatePicker>
+        <Button type='primary'>按钮</Button>
     </div>
 )
